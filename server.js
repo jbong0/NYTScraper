c1d310b7d5252a127ee47b47460ed2ce71059fa8
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-07-13 12:36:39 -0700

    Connected db

--- server.js
+++ server.js
@@ -6,14 +6,21 @@ const express = require('express'),
       logger = require('morgan'),
       path = require('path'),
       favicon = require('serve-favicon'),
-      PORT = process.env.PORT || 3000
+      PORT = process.env.PORT || 8080
 
 //initializing the app
 const app = express()
 
 // installize MongoDB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect('mongodb://newuser123:newuser123@ds235411.mlab.com:35411/heroku_h64kslnk')
+mongoose
+    .connect('mongodb://newuser123:newuser123@ds235411.mlab.com:35411/heroku_h64kslnk', {useNewUrlParser: true})
+    .then( result => {
+        console.log(`Connected to database '${result.connections[0].name}' on ${result.connections[0].host}:${result.connections[0].port}`);
+      })
+      .catch(err => console.log('There was an error with your connection:', err));
+
+//setting up favicon middleware
+app.use(favicon(path.join(__dirname, 'public', 'assets/img/favicon.ico')));
 
 //setting up Morgan middleware
 app.use(logger('dev'));
@@ -33,17 +40,17 @@ app.use('/notes',express.static(path.join(__dirname, 'public')));
 
 
 //setting up routes
-const index = require('./routes/index'),
-      articles = require('./routes/articles'),
-      notes = require('./routes/notes'),
-      scrape = require('./routes/scrape')
-      clear = require('./routes/clear');
+const index = require('./routes/index')
+    //   articles = require('./routes/articles'),
+    //   notes = require('./routes/notes'),
+    //   scrape = require('./routes/scrape')
+    //   clear = require('./routes/clear');
 
 app.use('/', index);
-app.use('/articles', articles);
-app.use('/notes', notes);
-app.use('/scrape', scrape);
-app.use('/clear', clear);
+// app.use('/articles', articles);
+// app.use('/notes', notes);
+// app.use('/scrape', scrape);
+// app.use('/clear', clear);
 
 app.listen(PORT, function(){
   console.log("Server listening on https://localhost:" + PORT)
